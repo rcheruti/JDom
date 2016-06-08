59d7e2a15f7a7d7b17674d0fa945b10752a46333
rcheruti <rcheruti@WGPH_WS4.wgph.com.br>
2016-06-08 16:11:18 -0300

    Iniciando repo. Subindo código pronto, será criado códigos de teste com Jasmine, e depois um novo Branch para remover as alterações dos protótipos do navegador (deixar apenas como o JQ).

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+
+module.exports = function (grunt) {
+  
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  
+  var src = 'public_html/',
+      dist = 'dist/';
+  
+  grunt.initConfig({
+    clean:[dist+'css'],
+    
+  });
+  
+  
+  grunt.registerTask('default',['clean']);
+  
+};
